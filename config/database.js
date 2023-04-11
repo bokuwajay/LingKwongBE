55a55f0415033f61b378c249aac50538afa4f167
Jay Li <jayl@peplink.com>
2023-04-11 17:37:36 +0800

    change mongo config to atlas

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
+        host: env('DATABASE_HOST', 'clusterlingkwong.ya4ybdd.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'db_lk_backend'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        username: env('DATABASE_USERNAME', 'bokuwajay'),
+        password: env('DATABASE_PASSWORD', 'Jay900515!'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
