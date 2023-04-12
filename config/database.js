fbb354e98004b628f9228819aaefe3f3065bf8a4
Jay Li <jayl@peplink.com>
2023-04-12 09:32:01 +0800

    test no db name

--- config/database.js
+++ config/database.js
@@ -4,15 +4,15 @@ module.exports = ({env}) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        // host: env('DATABASE_HOST', 'cluster0.49wcu1u.mongodb.net'),
-        // srv: env.bool('DATABASE_SRV', true),
-        // port: env.int('DATABASE_PORT', 27017),
-        // database: env('DATABASE_NAME', 'db_lk_backend'),
-        // username: env('DATABASE_USERNAME', 'bokuwajay'),
-        // password: env('DATABASE_PASSWORD', 'Jay900515!'),
-        uri: env(
-          'mongodb+srv://bokuwajay:Jay900515!@cluster0.49wcu1u.mongodb.net/db_lk_backend?retryWrites=true&w=majority',
-        ),
+        host: env('DATABASE_HOST', 'cluster0.49wcu1u.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', null),
+        username: env('DATABASE_USERNAME', 'bokuwajay'),
+        password: env('DATABASE_PASSWORD', 'Jay900515!'),
+        // uri: env(
+        //   'mongodb+srv://bokuwajay:Jay900515!@cluster0.49wcu1u.mongodb.net/db_lk_backend?retryWrites=true&w=majority',
+        // ),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
