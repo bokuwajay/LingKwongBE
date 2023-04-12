b9ade55dc2b9331980a484b5c578d36de98081e9
Jay Li <jayl@peplink.com>
2023-04-12 09:40:51 +0800

    test db

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({env}) => ({
         host: env('DATABASE_HOST', 'cluster0.49wcu1u.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', null),
+        database: env('DATABASE_NAME', 'db_lk_backend'),
         username: env('DATABASE_USERNAME', 'bokuwajay'),
         password: env('DATABASE_PASSWORD', 'Jay900515!'),
         // uri: env(
